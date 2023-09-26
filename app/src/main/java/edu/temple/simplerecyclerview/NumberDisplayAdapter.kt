7055209d34ab7cef62472652ae6d066f6b366ef2
Jcreasy00 <jcreasy44@gmail.com>
2023-09-26 15:18:33 -0400

    Added functions to NumberDisplayADapter including onCreateViewHolder. Also added code in main

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,14 +1,32 @@
 package edu.temple.simplerecyclerview
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* Step 3a: Provide Adapter Parent */ {
+class NumberDisplayAdapter (_numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
+    private var numbers = _numbers
     class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+        return NumberViewHolder(
+            TextView(
+                parent.context
+            ) .apply {
+                setPadding(5, 10, 0, 10)
+            }
+        )
+    }
+    override fun getItemCount(): Int = numbers.size
+
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+        holder.textView.text = numbers[position].toString()
+    }
     //Step 3b: Complete function definitions for adapter
 
 }
